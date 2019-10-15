abc77c7dbe1bb043c0cc51a81dbd7619a540f8e4
Romario Gustave <romagustave10@gmail.com>
2019-10-15 03:30:01 +0000

    Automatically backed up by Learn

--- lib/nds_explore.rb
+++ lib/nds_explore.rb
@@ -8,4 +8,8 @@ def pretty_print_nds(nds)
 end
 
 def print_first_directors_movie_titles
+ movies=[{:name =>"Jaws", :movie =>"Close Encounters of the Third Kind"}
+ {:name => "E.T", :movie => "Raiders of the Lost Ark"} 
+ the Extra-terrestrial"
+ \nSchindler's List\nLincoln\n"]
 end
