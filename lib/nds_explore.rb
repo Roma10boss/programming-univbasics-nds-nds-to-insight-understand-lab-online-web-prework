d4029083a0bf9f807a39a3e6f6588018b4f52c9e
Romario Gustave <romagustave10@gmail.com>
2019-10-15 03:44:33 +0000

    Automatically backed up by Learn

--- lib/nds_explore.rb
+++ lib/nds_explore.rb
@@ -7,10 +7,11 @@ def pretty_print_nds(nds)
   nil
   names=["Jaws", "E.T","Schindler"]
   movies= ["Close Encounters of the Third Kind","Close Encounters of the Third Kind", "the Extra-terrestrial list "  ]
+  a_o_a= [[1,2,3],[456],[7,8,9]]
   
-  movies=[{:name =>"Jaws", :movie =>"Close Encounters of the Third Kind"},
- {:name => "E.T", :movie => Close Encounters of the Third Kind"},
- {:name => "Schindler", :movie => "the Extra-terrestrial list "}]
+  pp name
+  pp movies
+  pp a_o_a
 end
 
 def print_first_directors_movie_titles
