92a9651472d0c36edc02d586cd54b2436f36838f
Romario Gustave <romagustave10@gmail.com>
2019-10-15 03:41:21 +0000

    Automatically backed up by Learn

--- lib/nds_explore.rb
+++ lib/nds_explore.rb
@@ -5,10 +5,16 @@ require 'directors_database'
 def pretty_print_nds(nds)
   # Change the code below to pretty print the nds with pp
   nil
+  names=["Jaws", "E.T","Schindler"]
+  movies= [""]
+  
+  movies=[{:name =>"Jaws", :movie =>"Close Encounters of the Third Kind"},
+ {:name => "E.T", :movie => "Raiders of the Lost Ark"},
+ {:name => "Schindler", :movie => " the Extra-terrestrial list "}]
 end
 
 def print_first_directors_movie_titles
  movies=[{:name =>"Jaws", :movie =>"Close Encounters of the Third Kind"},
  {:name => "E.T", :movie => "Raiders of the Lost Ark"},
- {:name => "Schindler" :movie => " the Extra-terrestrial list "}]
+ {:name => "Schindler", :movie => " the Extra-terrestrial list "}]
 end
